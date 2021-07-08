49009367029e8318b70e2193addcd13040623769
daffolap-159 <ankur.sharma@daffodilsw.com>
2021-07-08 12:23:59 +0530

    asdasd

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MyFramework",
+            dependencies: []),
         .testTarget(
             name: "MyFrameworkTests",
             dependencies: ["MyFramework"]),
